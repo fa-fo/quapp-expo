b7312ba881ace71b986a534944016d3483e110bb
fa-fo <79026313+fa-fo@users.noreply.github.com>
2023-07-20 08:14:16 +0200

    update to expo48

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const createExpoWebpackConfigAsync = require('@expo/webpack-config');
 
 module.exports = async function (env, argv) {
   const config = await createExpoWebpackConfigAsync(env, argv);
+
   // Customize the config before returning it.
-  config.optimization.usedExports = true; // important!!
+  config.mode = 'development';
+  //config.optimization.usedExports = true; // important!!
+
   return config;
 };
