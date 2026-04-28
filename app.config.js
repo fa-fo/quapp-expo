8e0389336af6efefa59b6df1e38cea80a13b2901
Frank <office@falbrecht.de>
2026-04-28 12:32:39 +0200

    update to version code 29006

--- app.config.js
+++ app.config.js
@@ -1,5 +1,5 @@
 const IS_DEV = process.env.APP_VARIANT === 'development';
-const versionCode = 29005;
+const versionCode = 29006;
 
 export default {
     name: IS_DEV ? 'DEV QuattFo Quapp' : 'QuattFo Quapp',
